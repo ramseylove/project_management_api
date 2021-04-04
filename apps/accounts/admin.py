255830749572d4a0d008a25a7b223acb0a073d71
Ryan Meyer <ryan@atriadev.com>
2021-04-04 15:03:12 -0600

    specified fieldsets for user in admin

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -14,14 +14,27 @@ class CustomUserAdmin(UserAdmin):
     model = CustomUser
     list_display = ['email', ]
     list_filter = ('is_staff', 'is_admin')
-    fieldsets = UserAdmin.fieldsets 
+    # fieldsets = UserAdmin.fieldsets
 
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Permissions', {'fields': ('is_admin', 'is_staff')}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            # 'classes': ('wide',),
+            'fields': ('email', 'password1', 'password2'),
+        }),
+    )
     search_fields = ('email',)
     ordering = ('email',)
     filter_horizontal = ()
 
+
 class UserProfileAdmin(admin.ModelAdmin):
     pass
+
 
 admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(UserProfile, UserProfileAdmin)
