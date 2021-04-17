8cec83356474554d39c286cb920d64b961a1649a
Ryan Meyer <ryan@atriadev.com>
2021-04-17 16:22:14 -0600

    fixed db issues, added entrypoint script to flush db and migrate on build

--- config/settings.py
+++ config/settings.py
@@ -112,10 +112,10 @@ WSGI_APPLICATION = 'config.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB', None),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASS', None),
-        'HOST': os.getenv('DB_HOST'),
+        'NAME': os.getenv('DB', 'postgres'),
+        'USER': os.getenv('DB_USER', 'postgres'),
+        'PASSWORD': os.getenv('DB_PASS', ''),
+        'HOST': os.getenv('DB_HOST', 'db'),
         'PORT': os.getenv('DB_PORT', '5432'),
 
     }
