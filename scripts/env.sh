fdde7d5708d6e969d41b4c91549402261e26a039
Ryan Meyer <ryan@atriadev.com>
2021-04-30 08:33:35 -0600

    edited allowed hosts to accept env variable

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,1 +1,4 @@
-dokku config:set --no-restart pmb SECRET_KEY="n^7y0lbvqx0(vpzfek)aptr2@n%-i%tgcu#7nfx9apu3_5fb%_" AWS_ACCESS_KEY_ID=OL3YTOGRAG36W4S76FA5 AWS_SECRET_ACCESS_KEY=vtAQiiWFxfFxZXajLiDreXf2p45KrChwGTlPR9rGGWs USE_S3=True
+dokku config:set --no-restart pmb SECRET_KEY="n^7y0lbvqx0(vpzfek)aptr2@n%-i%tgcu#7nfx9apu3_5fb%_"
+AWS_ACCESS_KEY_ID=OL3YTOGRAG36W4S76FA5
+AWS_SECRET_ACCESS_KEY=vtAQiiWFxfFxZXajLiDreXf2p45KrChwGTlPR9rGGWs
+USE_S3=True
